0560b24364e12b3792a9154556364b18074d328a
Joe Timmer <JosephTimmer90@Gmail.com>
2025-12-23 13:09:17 -0600

    Create game pieces

--- src/AppComponents/Game.js
+++ src/AppComponents/Game.js
@@ -3,7 +3,25 @@ import React from "react";
 
 function Game(){
     return (
-        <p>THis is where the game will go.</p>
+        <>
+            <section className="GameHeader">
+                <div>Score</div>
+                <div>Equipment</div>
+                <div>Level</div>
+            </section>
+
+            <section className="GamePieceArea">
+                <div>Alien</div>
+                <div>Laser</div>
+                <div>Turret</div>
+            </section>
+
+            <section className="GameFooter">
+
+            </section>
+        </>
+        
+            
     )
 }
 
